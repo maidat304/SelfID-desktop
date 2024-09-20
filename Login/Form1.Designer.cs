00d3399ec46aa8bce03023c849ebcf243bfc7550
meowmeow2301 <145837487+meowmeow2301@users.noreply.github.com>
2024-09-20 11:08:35 +0700

    CREATE: First Commit

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.account_now = new System.Windows.Forms.Label();
+            this.lblForgotPassword = new System.Windows.Forms.Label();
+            this.lblRegister = new System.Windows.Forms.Label();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnTogglePassword = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.account_now);
+            this.splitContainer1.Panel1.Controls.Add(this.lblForgotPassword);
+            this.splitContainer1.Panel1.Controls.Add(this.lblRegister);
+            this.splitContainer1.Panel1.Controls.Add(this.lblErrorMessage);
+            this.splitContainer1.Panel1.Controls.Add(this.btnLogin);
+            this.splitContainer1.Panel1.Controls.Add(this.btnTogglePassword);
+            this.splitContainer1.Panel1.Controls.Add(this.txtPassword);
+            this.splitContainer1.Panel1.Controls.Add(this.lblPassword);
+            this.splitContainer1.Panel1.Controls.Add(this.txtEmail);
+            this.splitContainer1.Panel1.Controls.Add(this.lblEmail);
+            this.splitContainer1.Panel1.Controls.Add(this.lblTitle);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1094, 572);
+            this.splitContainer1.SplitterDistance = 547;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // account_now
+            // 
+            this.account_now.AutoSize = true;
+            this.account_now.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.account_now.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(28)))), ((int)(((byte)(131)))));
+            this.account_now.Location = new System.Drawing.Point(287, 445);
+            this.account_now.Name = "account_now";
+            this.account_now.Size = new System.Drawing.Size(104, 20);
+            this.account_now.TabIndex = 10;
+            this.account_now.Text = "Đăng kí ngay !";
+            this.account_now.Click += new System.EventHandler(this.account_now_Click);
+            // 
+            // lblForgotPassword
+            // 
+            this.lblForgotPassword.AutoSize = true;
+            this.lblForgotPassword.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForgotPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(28)))), ((int)(((byte)(131)))));
+            this.lblForgotPassword.Location = new System.Drawing.Point(197, 478);
+            this.lblForgotPassword.Name = "lblForgotPassword";
+            this.lblForgotPassword.Size = new System.Drawing.Size(120, 20);
+            this.lblForgotPassword.TabIndex = 9;
+            this.lblForgotPassword.Text = "Quên mật khẩu ?";
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.AutoSize = true;
+            this.lblRegister.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(28)))), ((int)(((byte)(131)))));
+            this.lblRegister.Location = new System.Drawing.Point(146, 445);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(135, 20);
+            this.lblRegister.TabIndex = 8;
+            this.lblRegister.Text = "Chưa có tài khoản?";
+            this.lblRegister.Click += new System.EventHandler(this.lblRegister_Click);
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.AutoSize = true;
+            this.lblErrorMessage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorMessage.Location = new System.Drawing.Point(80, 310);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(162, 20);
+            this.lblErrorMessage.TabIndex = 7;
+            this.lblErrorMessage.Text = "* Mật khẩu sai, nhập lại";
+            this.lblErrorMessage.Visible = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(139)))), ((int)(((byte)(197)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(187, 349);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(174, 40);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnTogglePassword
+            // 
+            this.btnTogglePassword.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTogglePassword.Location = new System.Drawing.Point(420, 270);
+            this.btnTogglePassword.Name = "btnTogglePassword";
+            this.btnTogglePassword.Size = new System.Drawing.Size(40, 30);
+            this.btnTogglePassword.TabIndex = 5;
+            this.btnTogglePassword.UseVisualStyleBackColor = true;
+            this.btnTogglePassword.Click += new System.EventHandler(this.btnTogglePassword_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(80, 270);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.AutoSize = false;
+            this.txtPassword.Size = new System.Drawing.Size(340, 35);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+     //       this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(80, 240);
+            this.lblPassword.Name = "lblPassword";
+  //          this.lblPassword.Size = new System.Drawing.Size(82, 23);
+     //       this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Mật khẩu";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(80, 190);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.AutoSize = false;
+            this.txtEmail.Size = new System.Drawing.Size(340, 35);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // lblEmail
+            // 
+       //     this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(80, 160);
+            this.lblEmail.Name = "lblEmail";
+         ///   this.lblEmail.Size = new System.Drawing.Size(51, 23);
+        //    this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(28)))), ((int)(((byte)(131)))));
+            this.lblTitle.Location = new System.Drawing.Point(180, 80);
+            this.lblTitle.Name = "lblTitle";
+     //       this.lblTitle.Size = new System.Drawing.Size(200, 41);
+      //      this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "ĐĂNG NHẬP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 572);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnTogglePassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblErrorMessage;
+        private System.Windows.Forms.Label lblRegister;
+        private System.Windows.Forms.Label lblForgotPassword;
+        private System.Windows.Forms.Label account_now;
+        //    private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ Login/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+
+            #region "Size"
+            this.MinimumSize = new Size(800, 500); // default
+            this.MaximumSize = new Size(800, 500);
+            this.FormBorderStyle = FormBorderStyle.Sizable; // change size
+            this.DoubleBuffered = true;
+            #endregion
+
+            #region "Font"
+            lblEmail.ForeColor = ColorTranslator.FromHtml("#4A2574");
+            lblPassword.ForeColor = ColorTranslator.FromHtml("#4A2574");
+            txtEmail.ForeColor = ColorTranslator.FromHtml("#4A2574");
+            txtPassword.ForeColor = ColorTranslator.FromHtml("#4A2574");
+            txtEmail.BackColor = ColorTranslator.FromHtml("#E2DDFF");
+            txtPassword.BackColor = ColorTranslator.FromHtml("#E2DDFF");
+            lblErrorMessage.ForeColor = ColorTranslator.FromHtml("#87101E");
+
+            lblRegister.Font = new Font(lblRegister.Font, FontStyle.Italic);
+
+            account_now.Font = new Font(lblRegister.Font, FontStyle.Bold | FontStyle.Italic);
+
+            lblForgotPassword.Font = new Font(lblForgotPassword.Font, FontStyle.Italic);
+
+
+            this.Load += new EventHandler(Form1_Load);
+
+
+
+            #endregion
+
+            #region "button login"
+            // event màu gradient cho btnLogin
+
+            this.btnLogin.Paint += btnLogin_Paint;
+            this.btnLogin.MouseEnter += btnLogin_MouseEnter;
+            this.btnLogin.MouseLeave += btnLogin_MouseLeave;
+            // click effective
+
+            #endregion
+
+            #region "icon default"
+            string eyeOpenIconPath = @"image\eye.png"; 
+
+            btnTogglePassword.Image = Image.FromFile(eyeOpenIconPath);
+            btnTogglePassword.ImageAlign = ContentAlignment.MiddleCenter;
+            btnTogglePassword.Size = new Size(31, 27);
+            #endregion
+
+
+        }
+
+        #region "font"
+        private PrivateFontCollection privateFonts = new PrivateFontCollection();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                CustomizeControls();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tùy chỉnh giao diện: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            string fontPath = @"font\Inter_18pt-Regular.ttf";
+
+           
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile(fontPath);
+
+            Font interFont = new Font(pfc.Families[0], 10f, FontStyle.Regular); 
+
+      
+            lblTitle.Font = new Font(pfc.Families[0], lblTitle.Font.Size, lblTitle.Font.Style);
+            lblEmail.Font = new Font(pfc.Families[0], lblEmail.Font.Size, lblEmail.Font.Style);
+            lblPassword.Font = new Font(pfc.Families[0], lblPassword.Font.Size, lblPassword.Font.Style);
+            btnLogin.Font = new Font(pfc.Families[0], btnLogin.Font.Size, btnLogin.Font.Style);
+            lblErrorMessage.Font = new Font(pfc.Families[0], lblErrorMessage.Font.Size, lblErrorMessage.Font.Style);
+            lblRegister.Font = new Font(pfc.Families[0], lblRegister.Font.Size, lblRegister.Font.Style);
+            lblForgotPassword.Font = new Font(pfc.Families[0], lblForgotPassword.Font.Size, lblForgotPassword.Font.Style);
+            account_now.Font = new Font(pfc.Families[0], account_now.Font.Size, account_now.Font.Style);
+            btnTogglePassword.Font = new Font(pfc.Families[0], btnTogglePassword.Font.Size, btnTogglePassword.Font.Style);
+        }
+
+
+
+        #endregion
+
+        #region "right"
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+            // màu gradient
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.FromArgb(108, 139, 197),
+                Color.FromArgb(88, 28, 131),  
+                102.92f)) // Góc gradient
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+        #endregion
+
+
+        #region "left"
+        private void CustomizeControls()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(CustomizeControls));
+                return;
+            }
+
+            SafeCustomize(txtEmail, CustomizeTextBox);
+            SafeCustomize(txtPassword, CustomizeTextBox);
+            SafeCustomize(btnLogin, CustomizeButton);
+            SafeCustomize(btnTogglePassword, CustomizeToggleButton);
+        }
+
+        private void SafeCustomize<T>(T control, Action<T> customizeAction) where T : Control
+        {
+            if (control != null && !control.IsDisposed)
+            {
+                customizeAction(control);
+            }
+        }
+
+        private void CustomizeTextBox(TextBox textBox)
+        {
+            textBox.BorderStyle = BorderStyle.None;
+            textBox.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, textBox.Width, textBox.Height, 8, 8));
+   
+        }
+
+        private void CustomizeButton(Button button)
+        {
+            button.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, button.Width, button.Height, 8, 8));
+        }
+
+        private void CustomizeToggleButton(Button button)
+        {
+            button.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, button.Width, button.Height, 8, 8));
+        }
+
+        [System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+       
+            if (txtPassword.Text == "123")
+            {
+                lblErrorMessage.Visible = false;
+                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                lblErrorMessage.Visible = true; 
+            }
+        }
+
+        private void btnTogglePassword_Click(object sender, EventArgs e)
+        {
+            // Toggle ẩn/hiện
+            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+
+            string eyeOpenIconPath = @"image\eye.png"; 
+            string eyeClosedIconPath = @"image\hidden.png"; 
+            btnTogglePassword.Image = Image.FromFile(eyeOpenIconPath);
+
+            if (txtPassword.UseSystemPasswordChar)
+            {
+                btnTogglePassword.Image = Image.FromFile(eyeOpenIconPath);
+            }
+            else
+            {
+
+                btnTogglePassword.Image = Image.FromFile(eyeClosedIconPath);
+            }
+
+            btnTogglePassword.ImageAlign = ContentAlignment.MiddleCenter;
+            btnTogglePassword.Size = new Size(31, 27); 
+        }
+
+
+       
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Capture = false; 
+                Message msg = Message.Create(this.Handle, 0xA1, new IntPtr(2), IntPtr.Zero);
+                this.WndProc(ref msg); 
+            }
+        }
+
+        #region "button login (color)"
+        private bool isMouseOverButton = false;
+        private void btnLogin_MouseEnter(object sender, EventArgs e)
+        {
+            isMouseOverButton = true;
+            btnLogin.Invalidate(); 
+        }
+
+        private void btnLogin_MouseLeave(object sender, EventArgs e)
+        {
+            isMouseOverButton = false;
+            btnLogin.Invalidate(); 
+        }
+
+        private void btnLogin_Paint(object sender, PaintEventArgs e)
+        {
+            Button btn = sender as Button;
+            Color startColor = isMouseOverButton
+                ? ColorTranslator.FromHtml("#6C8BC5") 
+                : ColorTranslator.FromHtml("#D69BFD"); 
+
+            Color endColor = isMouseOverButton
+                ? ColorTranslator.FromHtml("#581C83") 
+                : ColorTranslator.FromHtml("#9672FF"); 
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                btn.ClientRectangle,
+                startColor,
+                endColor,
+                102.92f)) 
+            {
+                e.Graphics.FillRectangle(brush, btn.ClientRectangle);
+            }
+
+            TextRenderer.DrawText(e.Graphics, btn.Text, btn.Font, btn.ClientRectangle, btn.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        }
+        #endregion
+
+
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+      
+        }
+
+
+        private void lblRegister_Click(object sender, EventArgs e)
+        {
+    
+        }
+
+
+        private void account_now_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        #endregion
+
+        private void lblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
